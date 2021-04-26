bc61915833a22bf61ff4ee3869ff7c6bfc707793
randomness-exe <71343868+randomness-exe@users.noreply.github.com>
2021-04-26 16:41:15 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
  $test = ($_POST['test']);
  $dasdas = "add to file";
- $handle = fopen('test.log', 'a');
+ $handle = fopen('save.log', 'a');
  fwrite($handle, $dasdas);
  fclose($handle);
  
